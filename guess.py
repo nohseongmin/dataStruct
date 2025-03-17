6c3da2dd377524656a9f2634f6a0279b919dbe24
nohseongmin <sm9375693@gmail.com>
2025-03-17 19:36:06 +0900

    v.03 O(log n) guess game

---
+++ guess.py
@@ -0,0 +1,20 @@
+import random
+
+answer = random.randrange(1, 1000)
+win = False
+for guesses in range(10):
+    guess = int(input(f"{10-guesses}번 남음, 수 입력 : "))
+    if answer == guess:
+        print("정답")
+        win = True
+        break
+    elif answer > guess:
+        print("입력한 수는 정답 보다 작음\n")
+    else:
+        print("입력한 수는 정답 보다 큼\n")
+    guesses = guesses + 1
+
+if win==True:
+    print("당신이 이겼습니다")
+if win==False:
+    print("패배")
