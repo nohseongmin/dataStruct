a02ee140443a9e15043e9e4e41294f0cee9ceda7
nohseongmin <sm9375693@gmail.com>
2025-03-24 17:38:53 +0900

    v0.7 zip

--- week03.py
+++ week03.py
@@ -1,20 +1,5 @@
-def move_zeros(l):
-    zero_idx = 0
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if zero_idx != i:
-                l[i] = 0
-            zero_idx = zero_idx + 1
-    return l
+groups = ['NEWJEANS', "promise9", "aespa"]
+rating = [1,2,3]
 
-l = [99, 0, -7, 0, 16]
-move_zeros(l)
-print(l)
-
-# import array
-#
-# arr = array.array('f', [99, 0, -7, 0, 16])
-# for i in range(len(arr)):
-#     print(f"{arr[i]:3} {id(arr[i])}")
+group_rating = list(zip(groups, rating))
+print(group_rating)
