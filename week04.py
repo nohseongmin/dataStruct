269a25d6e0a93e9c0289b0edc9ecee9d209b922c
nohseongmin <sm9375693@gmail.com>
2025-03-31 17:44:32 +0900

    v0.14 linked-list

--- week04.py
+++ week04.py
@@ -6,6 +6,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
     #내가만든 링크드 리스트에 값을 넣을 함수
     def append(self, data):
         if not self.head:
@@ -17,13 +18,25 @@ class LinkedList:
             current = current.link
         current.link = Node(data)
 
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if target == current.data:
+                return f"{target}을(를) 찾았습니다."
+            else:
+                current = current.link
+        return f"{target}은(는) 존재하지 않습니다."
+
     def __str__(self):
         current = self.head
         result = ""
         while current is not None:
-            result = result + str(current.data) + "->"
+            #result = result + str(current.data) + "->"
+            result = result + f"{current.data} -> "
             current = current.link
         return result
+
+
 #리스트생성 -> init메소드 동작 -> 링크드리스트 객체 생성, head 속성(none값) 들어감
 ll = LinkedList()
 #data값에 8이 전달, head가 none이므로 if문 동작, self.head에 새로 생긴 노드(8)의 주소값 넣음, 이후 리턴
@@ -33,3 +46,5 @@ ll.append(8)
 ll.append(10)
 ll.append(9)
 print(ll)
+print(ll.search(10))
+print(ll.search(11))
