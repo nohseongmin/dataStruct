1e975823f791de90682d4d7f23af2058c34bf7b9
nohseongmin <sm9375693@gmail.com>
2025-03-31 17:18:07 +0900

    v0.13 linked-list

--- week03.py
+++ week03.py
@@ -1,13 +1,13 @@
-def inters(l1, l2):
-    s1 = set(l1)
-    s2 = set(l2)
-    #return list(s1.intersection(s2)) #교집합
-    #return list(s1.union(s2))        #합집합
-    #return list(s1 & s2)             #차집합 다른 집합도 + / 등 사용가능
-    return list(s1.difference(s2))    #차집합
+def my_zip(l1, l2):
+    l3 = list()
+    length = 0
+    if len(l1)<len(l2):
+        length = len(l1)
+    else:
+        length = len(l2)
+    for i in range(length):
+        l3.append((l1[i], l2[i]))
+    return l3
 
 
-l1 = [45, 5, 22, 31, 7, 19]
-l2 = [22, 1, 4, 2, 5, 23, 30, 13, 41, 19]
-print(inters(l1,l2))
 
---
+++ week04.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    #내가만든 링크드 리스트에 값을 넣을 함수
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        #현재 노드의 링크
+        while current.link:
+            current = current.link
+        current.link = Node(data)
+
+    def __str__(self):
+        current = self.head
+        result = ""
+        while current is not None:
+            result = result + str(current.data) + "->"
+            current = current.link
+        return result
+#리스트생성 -> init메소드 동작 -> 링크드리스트 객체 생성, head 속성(none값) 들어감
+ll = LinkedList()
+#data값에 8이 전달, head가 none이므로 if문 동작, self.head에 새로 생긴 노드(8)의 주소값 넣음, 이후 리턴
+ll.append(8)
+#if문 동작 X, current (지역변수)에 self.head의 주소값 대입, current.link는 none이므로 while문 생략,
+#current.link에 Node(10) 넣음. 10의 주소값
+ll.append(10)
+ll.append(9)
+print(ll)
