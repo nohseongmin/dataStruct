b31abbee91d6292545ebd4ee74ca10d0da8a3fc8
nohseongmin <sm9375693@gmail.com>
2025-04-07 18:17:41 +0900

    v0.23 stack

--- week05.py
+++ week05.py
@@ -1,11 +1,19 @@
-s1 = []
-#push
-s1.append("data_Structure")
-s1.append("data_Base")
-#peek
-print(s1)
-print(s1[-1])
-#pop
-print(s1)
-print(s1.pop())
-print(s1)
+#print(1+3))
+def is_valid_parentheses(expression : str) -> bool: #type hint 사용(강제아님)
+    stack = list()
+    for letter in expression:
+        #기본 아이디어:(에서 push )에서 pop.
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+print(is_valid_parentheses(")(1+2))")) #예상) f
+print(is_valid_parentheses("(1+2))")) # f
+print(is_valid_parentheses("(1+2)")) #t
+print(is_valid_parentheses("((3*2)/+2)")) #t
+
