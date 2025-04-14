4c610cb92517bbe0e49e461bf9605f5f6b5f73b3
nohseongmin <sm9375693@gmail.com>
2025-04-14 18:02:51 +0900

    v0.27 queue

--- week06.py
+++ week06.py
@@ -1,36 +1,9 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size=0
-    def enqueue(self, data):
-        self.size = self.size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node #move
+from queue import Queue
 
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError("queue is MT")
-        self.size = self.size-1
-        temp = self.front
-        self.front = self.front.link # 이동
-        temp.link = None # 주소 초기화(링크 끊기)
-        if self.front is None:
-            self.rear = None
-        return temp.data
 q = Queue()
-q.enqueue("자료구조")
-q.enqueue("DB")
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
+q.put("자료구조")
+q.put("DB")
+print(q.qsize())
+print(q.get())
+print(q.qsize())
+print(q.get())
