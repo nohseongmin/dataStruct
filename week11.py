c1c2230bf5adf73e2c0b2c4f4467a941ca9ecb04
nohseongmin <sm9375693@gmail.com>
2025-05-19 18:03:10 +0900

    v1.4 graph+dfs

--- week11.py
+++ week11.py
@@ -1,41 +1,24 @@
-class Graph:
-    def __init__(self, size):
-        self.SIZE = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
 
-    def print_Graph(self, size):
-        for r in range(size):
-            for c in range(size):
-                print(self.graph[r][c], end=' ')
-            print()
+def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g,j,visited)
+def bfs(g,i,visited):
+    pass
 
-
-G1 = Graph(4)
-G3 = Graph(4)
-G_self = Graph(4)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-
-print("G1 무방향 그래프")
-G1.print_Graph(G1.SIZE)
-
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G3.graph[0][1] = 1; G3.graph[0][2] = 1
-G3.graph[3][0] = 1; G3.graph[3][2] = 1
-
-print("G3 방향 그래프")
-G1.print_Graph(G3.SIZE)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G_self.graph[0][3] = 1
-G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-G_self.graph[2][1] = 1
-G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-print("G_self 무방향 그래프")
-G_self.print_Graph(G_self.SIZE)
+visited_dfs = [0 for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 7, visited_dfs); print()
+bfs(graph, 4, visited_bfs)
